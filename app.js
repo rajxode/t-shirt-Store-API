6399a4cd84ab974ec9eb4bbc05800030782e618c
rajxode <rajsh1202@gmail.com>
2023-09-15 21:56:56 +0530

    add morgan logger and swagger docs for api documentation

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+
 
 const app = express();
+
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}));
+
+app.use(cookieParser());
+app.use(fileUpload());
+
+app.use(morgan('tiny'));
+
 
 module.exports = app;
