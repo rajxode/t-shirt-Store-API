3c76824119ff904b1854d7b27c430b2d05d9e853
rajxode <rajsh1202@gmail.com>
2023-09-23 14:50:23 +0530

    create route and controller for product update,delete and get list of product

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,7 +14,7 @@ const CustomError = require('../utils/customError');
 const WhereClause = require('../utils/whereClause');
 
 
-// controller for adding a new product
+// controller for Admin to add a new product
 module.exports.addProduct = BigPromise(async(req,res,next) => {
 
     // checking whether user is uploading some images or not
@@ -90,7 +90,147 @@ module.exports.addProduct = BigPromise(async(req,res,next) => {
 
 })
 
+// controller for Admin to get list of all the products
+module.exports.adminGetProducts = BigPromise(async(req,res,next) => {
 
+    const products = await Product.find();
+
+    // finally return the data
+    res.status(200).json({
+        success:true,
+        products,
+    })
+});
+
+// controller for admin to update any product by id
+module.exports.adminUpdateOne = BigPromise(async(req,res,next) => {
+
+    // finding product
+    let product = await Product.findById(req.params.id);
+
+    // if no product found
+    if(!product){
+        return next(new CustomError('No product found' , 401));
+    }
+
+    // if user wants to update images also
+    if(req.files){
+
+        // create new array for images
+        let imageArray = [];
+
+        // first remove the previously saved images
+        for(let i = 0; i < product.photos.length; i++){
+            // get file id from photo array
+            const file = product.photos[i].id;
+        
+            // remove each file
+            await cloudinary.uploader.destroy(file)
+        }
+
+
+        // now add the new images
+        // incase user send more than 1 image
+        if( req.files.photos.length > 1){
+
+            // map over each image
+            for(let i = 0; i < req.files.photos.length; i++){
+
+                // get every file
+                const file = req.files.photos[i];
+                
+                // uploading each file
+                const result = await cloudinary.uploader.upload(file.tempFilePath,{
+                    folder:process.env.CLOUD_PRODUCT_FOLDER
+                })
+
+                // push result of uploaded image in array
+                imageArray.push(
+                    {
+                        id:result.public_id,
+                        secure_url:result.secure_url
+                    }
+                )
+            }
+        }
+
+        // incase user send only one image
+        // seperate function for single image because single image comes as an object and not as an array
+        else{
+            
+            // get the file
+            const file = req.files.photos;
+            
+            // uploading 
+            const result = await cloudinary.uploader.upload(file.tempFilePath,{
+                folder:process.env.CLOUD_PRODUCT_FOLDER
+            })
+
+            // push result of uploaded image in array
+            imageArray.push(
+                {
+                    id:result.public_id,
+                    secure_url:result.secure_url
+                }
+            )
+        }
+
+        // store the array of new uploaded images inside req.body
+        req.body.photos = imageArray;
+    }
+
+    // update the product data with the value provided by the user
+    product = await Product.findByIdAndUpdate(
+                            // product id
+                            req.params.id,
+                            // data to be updated
+                            req.body,
+                            {
+                                new:true,
+                                runValidators:true,
+                                useFindAndModify: false,
+                            }
+                        );
+
+    // finally return the data
+    res.status(200).json({
+        success:true,
+        product,
+    })
+});
+
+// controller for admin to update any product by id
+module.exports.adminDeleteOne = BigPromise(async(req,res,next) => {
+
+    // finding product
+    let product = await Product.findById(req.params.id);
+
+    // if no product found
+    if(!product){
+        return next(new CustomError('No product found' , 401));
+    }
+
+    // first remove the previously saved images
+    for(let i = 0; i < product.photos.length; i++){
+        // get file id from photo array
+        const file = product.photos[i].id;
+        // remove each file
+        await cloudinary.uploader.destroy(file)
+    }
+
+    // find the product and then delete it
+    await Product.findByIdAndDelete(req.params.id);
+
+    // finally return the data
+    res.status(200).json({
+        success:true,
+        message:'Product is deleted'
+    })
+});
+
+
+
+// for all the users to get list of all the products
 module.exports.getAllProducts = BigPromise(async(req,res,next) => {
 
     // show product on each page
@@ -132,3 +272,17 @@ module.exports.getAllProducts = BigPromise(async(req,res,next) => {
         totalProduct
     })
 });
+
+// for all the users to get a single product by it's id
+module.exports.getOneProduct = BigPromise(async(req,res,next) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new CustomError('No product found', 401));
+    }
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+})
--- routes/product.js
+++ routes/product.js
@@ -11,9 +11,27 @@ const productController = require('../controllers/productController');
 // middleware to check whether user is loggedIn or not
 const {isLoggedIn, customRole} = require('../middlewares/user');
 
+
+// =========== for just admins ===========
+
 // route for adding a product
 router.route('/addProduct').post(isLoggedIn, customRole('admin'), productController.addProduct);
+// route for getting list of all the products
+router.route('/adminGetProducts').get(isLoggedIn, customRole('admin'), productController.adminGetProducts)
 
+router.route('/:id')
+        // for updating the product
+        .put(isLoggedIn, customRole('admin'), productController.adminUpdateOne)
+        // for deleting the product
+        .delete(isLoggedIn, customRole('admin'), productController.adminDeleteOne)
+
+// ========= for all users ==========
+
+// get list of all the products
 router.route('/getAllProducts').get(isLoggedIn, productController.getAllProducts);
+// get data of a single product by it's Id
+router.route('/getOneProduct/:id').get(isLoggedIn, productController.getOneProduct)
+
+
 // export route
 module.exports = router;
